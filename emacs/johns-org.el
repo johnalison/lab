659b318806656b65488d6e4141946bc2de76abc8
John Alison <johnda102@gmail.com>
2025-05-03 15:28:09 -0400

    Got the autocomplete working

--- emacs/johns-org.el
+++ emacs/johns-org.el
@@ -114,7 +114,8 @@
             ((org-agenda-overriding-header "Open Items")))
       (todo "Waiting"
             ((org-agenda-overriding-header "Waiting on")))
-      (tags-todo "agenda/Waiting" ((org-agenda-overriding-header "Waiting on")))))
+      ;;(tags-todo "agenda/Waiting" ((org-agenda-overriding-header "Waiting on")))
+      ))
 
     ("t" "To do"
      ((todo "TODO"
@@ -246,23 +247,17 @@
 (define-prefix-command 'org-roam-prefix-map)
 (global-set-key (kbd "C-c n") 'org-roam-prefix-map)
 
+
 (use-package org-roam
   :ensure t
   :custom
-  (org-roam-directory (file-truename "~/RoamNotes"))
+  (org-roam-directory "~/RoamNotes")
   (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
          ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today)
-	 :map org-mode-map
+         :map org-mode-map
          ("C-M-i"    . completion-at-point))
   :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
+  (org-roam-setup))
+(org-roam-db-autosync-mode)
